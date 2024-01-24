00acebb2455acca2c333d0104abff647d117816c
Amank-07 <ui22cs05@iiitsurat.ac.in>
2024-01-24 23:45:21 +0530

    video 5 completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+let name="aman"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div classNameName="App">
+    //   <header classNameName="App-header">
+    //     <img src={logo} classNameName="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       classNameName="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       follow aman
+    //     </a>
+    //   </header>
+    // </div>
+
+
+    // <>
+    // <nav>
+    //   <li>HOme</li>
+    //   <li>About</li>
+    //   <li>contact</li>
+    // </nav>
+    // <div classNameName='container'>
+    //   <h1>hello {name}</h1>
+    // <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet dolorum magnam maiores veritatis deserunt dignissimos, autem, pariatur asperiores corrupti vero, quod placeat quae praesentium incidunt cupiditate facilis obcaecati hic. Rerum!</p>
+    // </div>
+    // </>
+    // <div classNameName='blank'>Lovely</div>
+
+    <>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href='/'>TextUtils</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href='/'>Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href='/'>About</a>
+      </li>
+      
+    </ul>
+    <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav> 
+    </>
+
   );
 }
 
